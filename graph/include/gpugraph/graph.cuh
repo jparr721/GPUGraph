3f986ce9bd5b8042c94cbf3c3dd368bd820d561d
Jarred Parr <jparr721@gmail.com>
2019-08-13 14:19:33 -0600

    fix up some of the graph ops

--- graph/include/gpugraph/graph.cuh
+++ graph/include/gpugraph/graph.cuh
@@ -2,32 +2,36 @@
 
 #include <string>
 #include <map>
+#include <memory>
 #include <vector>
 
 #include <gpugraph/tensor.cuh>
 
 namespace graph {
-  // Graph Node
-  typedef struct {
-    std::string name;
-    unsigned int connections;
-    tensor data;
-  } g_node;
+  constexpr int max_nodes = 20;
+  constexpr int max_connections = 40;
 
-  struct graph_memory {
-    float* graph;
-    float* edges;
-    int n_edges;
-    int n_nodes;
+  // Graph Nodes represent an entire neural network weight structure
+  struct g_node {
+    std::string name;
+    tensor data;
+  };
+
+  struct node {
+    g_node key;
+    g_node connections[max_connections];
+    int node_pos;
+  };
+
+  struct digraph {
+    node nodes[max_nodes];
   };
 
   class graph {
     public:
-      using adjacency_list = std::map<g_node, std::vector<g_node>>;
-
       graph & operator=(const graph&) = delete;
       graph(const graph&) = delete;
-      explicit graph(size_t max_capacity) : max_capacity_(max_capacity) {}
+      graph() {}
 
       ~graph();
 
@@ -40,22 +44,23 @@ namespace graph {
 
       bool built() { return is_built_; }
 
-      adjacency_list get_digraph() { return digraph_; }
+      digraph get_digraph() { return digraph_; }
     private:
       // Our block size
       constexpr static int block_size = 64;
 
       bool is_built_ = false;
 
-      // Initialize our device bound shared memory block
-      graph_memory d_mem_;
-
       // Make our adjacency list
-      adjacency_list digraph_;
+      std::unique_ptr<digraph> digraph_;
 
-      size_t max_capacity_ = 0;
+      // ========================================================
+      int find_key_pos(const g_node& new_node);
+
+      // ========================================================
+      bool key_exists(const g_node& new_node);
+
+      // ========================================================
+      bool has_edge(const g_node& source, const g_node& destination);
   };
-
-  __global__ void kadd_edge(graph_memory mem, int x1, int y1, int x2, int y2);
-  __global__ void kadd_node(graph_memory mem, int x, int y, float data);
 } // namespace graph
--- graph/src/graph.cu
+++ graph/src/graph.cu
@@ -1,5 +1,8 @@
 #include <cassert>
 #include <iostream>
+#include <stack>
+#include <utility>
+
 #include <gpugraph/graph.cuh>
 
 namespace graph {
@@ -9,35 +12,82 @@ namespace graph {
     // We want to build after all nodes are added
     assert(!digraph_.empty());
 
-    // Initialize our graph memory into the GPU
-    cudaMalloc(&d_mem_.graph, d_mem_.n_nodes);
-
-    // # of edges will be dynamic, but default to 3 * max_cap
-    cudaMalloc(&d_mem_.edges, d_mem_.n_edges);
+    // Make our shared pointer
+    digraph_ = std::make_shared<digraph>;
   }
 
   void graph::safe_add_node(const g_node& new_node) {
-    auto found = digraph_.find(new_node);
+    bool found = false;
+    for (int i = 0; i < digraph_->node_pos; ++i) {
+      if (digraph_->nodes[i] != NULL && digraph_->nodes[i] == new_node) {
+        found = true;
+      }
+    }
 
-    if (found == digraph_.end()) {
-      digraph_.insert({ new_node, std::vector<g_node>() });
+    if (!found && digraph_->node_pos < max_nodes) {
+      digraph_->nodes[digraph_->node_pos] = new_node;
+      ++digraph_->node_pos;
     }
   }
 
   void graph::unsafe_add_node(const g_node& new_node) {
-    digraph_.insert({ new_node, std::vector<g_node>() });
+    digraph_->nodes[digraph_->node_pos] = new_node;
   }
 
   void graph::add_edge(const g_node& source, const g_node& new_node) {
     // If graph is built, fail
     assert(!is_built_);
 
-    digraph_[source].push_back(new_node);
+    // We don't want cycles between neural networks in this case,
+    // always feed forward.
+    if (!has_edge(source, new_node)) {
+      int idx = find_key_pos(source);
+      int new_idx = digraph_->nodes[idx].node_pos;
+
+      digraph_->nodes[idx][++new_idx] = new_node;
+      ++digraph_->nodes[idx].node_pos;
+    }
   }
 
-  graph::~graph() {
-    // Clean up our graph memory
-    cudaFree(d_mem_.graph);
-    cudaFree(d_mem_.edges);
+  // ========================================================
+  bool graph::key_exists(const g_node& node) {
+    for (int kk = 0; kk < digraph_->nodes[kk].node_pos; ++kk) {
+      if (digraph_->nodes[kk].key == node) {
+        return true;
+      }
+    }
+
+    return false;
   }
+
+  // ========================================================
+  ///
+  /// Searches for an edge in a depth-first manner
+  ///
+  bool graph::has_edge(const g_node& source, const g_node& destination) {
+    std::stack<std::pair<g_node, in>> history;
+    std::vector<g_node> visited;
+
+    history.push({digraph_->nodes[idx], 0});
+
+    int idx = 0;
+    while !history.empty() {
+      // TODO
+    }
+  }
+
+  // ========================================================
+  int graph::find_key_pos(const g_node& node) {
+    for (int kk = 0; kk < digraph_->nodes[kk].node_pos; ++kk) {
+      if (digraph_->nodes[kk] == node) {
+        return kk;
+      }
+    }
+
+    return -1;
+  }
+
+  // ========================================================
+
+  graph::~graph() {}
 } // namespace graph
